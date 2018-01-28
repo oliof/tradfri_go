7158468c2ac43c004b8d16c5f26b97f7a65ffc61
oliof <wagener@gmail.com>
2018-01-28 09:37:45 +0000

    Add constants for color temp (as reported by lamps when switching using the tradfri app), dim value range, device types

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+// Derived from https://github.com/eclipse/smarthome/blob/4204ce06bb28c28e5f711e720f87ef83beff2e27/extensions/binding/org.eclipse.smarthome.binding.tradfri/src/main/java/org/eclipse/smarthome/binding/tradfri/TradfriBindingConstants.java
+
+
+const Remote = 0
+const Lamp = 2
+const DimMax = 254
+const DimMin = 0
+const ColorTempColdX = 24841
+const ColorTempColdY = 24593
+const ColorTempDayX = 29969
+const ColorTempDayY = 26804
+const ColorTempWarmX = 32977
+const ColorTempWarmY = 27105
